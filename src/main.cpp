842327c8d14935bd85baef26e5d1a1e3f0376aa0
Adam Cwiek <adam.cwiek@electronsquare.com>
2019-10-24 13:05:26 +0200

    comments in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,38 @@
 #include "regs/START.h"
 #include "regs/RegMap.h"
 
-static Register<REG_START, REG_START_NAME, ADDR_REG_START> START_REG;
+static Register<REG_START, REG_START_NAME, REG_START_ADDR> START_REG;
 
 int main() {
   {
+    // get address, register name and description
     uint32_t addr  = START_REG.get_address();
-    printf("%s, address 0x%x\n", get_register_name(addr), addr);
-    printf("%s, description %s\n", get_register_name(addr), get_register_description(addr));
+    printf("%s, addr: 0x%x\n", get_register_name(addr), addr);
+    printf("%s, desc: \"%s\"\n", get_register_name(addr), get_register_description(addr));
   }
 
-  {
+  { // assign hex value to register, and check if value changed
     START_REG = 0x03;
-    printf("START_REG value changed by assignment to 0x03\n");
+
+    printf("%s changed by assignment to 0x03\n", START_REG.get_name());
     if (START_REG == 0x03) {
-      printf("START_REG == 0x03\n");
+      printf("%s == 0x03\n", START_REG.get_name());
     }
     printf("changed: %d\n", START_REG.changed());
   }
 
-  {
+  { // assign value to bit-field, and check if value changed
     START_REG.reg.BIT_FUNC_E = 1;
     printf("START_REG.reg.BIT_FUNC_E value changed to 1 \n");
     printf("changed: %d\n", START_REG.changed());
   }
 
-  {
+  { // make no changes, and check if value changed
     printf("make no changes\n");
     printf("changed: %d\n", START_REG.changed());
   }
 
-  {
-    uint8_t val = START_REG;
-    printf("Register name : %s, value %d\n", START_REG.get_name(), val);
+  { // print value
     START_REG.print();
     START_REG.binary_print();
     START_REG.reg.debug_print();
